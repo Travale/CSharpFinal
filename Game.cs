515b4f82f66d03a54f7eaf001fe8ffe0b50e2938
Travis <supafatcats@hotmail.com>
2016-11-17 23:33:44 -0700

    New game

---
+++ Game.cs
@@ -0,0 +1,26 @@
+//Want battalions
+//weapon variable, speed, health, damage, luckiness
+//abilities for seperate battalions, Rasengan, Moon strike, leaf storm, light beam.
+//2 levels maybe 1. Deep forest and Rainbow caves. 
+//lose 5 hitpoints random battalion member dies.
+//Power ups.  Berserk and go meta
+//Next monster gets bigger than last. 
+//3 ninjas have a combo ability if one dies the power lessons.  If two ninjas have two different weapons they can attack with a combo.
+ 
+using System;
+
+namespace NinjaGame
+{
+    public class Game
+    {
+        public Game()
+        {
+            
+        }
+        public void Start()
+        {
+
+        }
+    }
+
+}
